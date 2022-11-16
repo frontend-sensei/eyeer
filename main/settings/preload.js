65245558fc96d594c1714a86aeb12ec9334fde1f
frontend-sensei <frontendsensei612@gmail.com>
2022-11-16 15:57:03 +0200

    Refactor preload for settings

--- main/settings/preload.js
+++ main/settings/preload.js
@@ -1,12 +1,7 @@
 const { contextBridge, ipcRenderer } = require("electron");
 
-const validChannels = ["main", "get-settings-data"];
+const validChannels = ["get-settings-data"];
 contextBridge.exposeInMainWorld("api", {
-  send: (channel, data) => {
-    if (validChannels.includes(channel)) {
-      ipcRenderer.send(channel, data);
-    }
-  },
   receive: (channel, func) => {
     if (validChannels.includes(channel)) {
       ipcRenderer.on(channel, (_event, ...args) => func(...args));
