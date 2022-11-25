3a5d853bf8a3bc67de4923651168380c4c5c2dd5
frontend-sensei <frontendsensei612@gmail.com>
2022-11-25 16:46:26 +0200

    Add conditional tray icon loading

--- main/app-setup/tray/tray.js
+++ main/app-setup/tray/tray.js
@@ -3,6 +3,8 @@ const path = require("path");
 const createSettingsWindow = require("../../settings/createSettingsWindow");
 const handleBreakEnd = require("../../break/handleBreakEnd");
 const setupBreak = require("../../break/setupBreak");
+const isWindows = require("../../utils/isWindows");
+const isLinux = require("../../utils/isLinux");
 
 const setupTray = () => {
   const windows = {
@@ -11,8 +13,22 @@ const setupTray = () => {
   const brakerController = {
     isPaused: false,
   };
+  const iconExtensions = {
+    windows: "ico",
+    linux: "png",
+  };
+  const getIconExtension = () => {
+    if (isWindows) {
+      return iconExtensions.windows;
+    }
+    if (isLinux) {
+      return iconExtensions.linux;
+    }
+    // TODO: For macOS need to generate Template Image
+    return "png";
+  };
   const tray = new Tray(
-    path.resolve(__dirname, "../../assets/braker-icon.png")
+    path.resolve(__dirname, `../../assets/braker-icon.${getIconExtension()}`)
   );
   app.whenReady().then(buildTray);
 
